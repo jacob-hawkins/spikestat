e5d3c5ee61040213811ad34fca0e257f6a28c4a6
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-05-27 14:40:50 -0400

    context

---
+++ client/src/context/AuthActions.js
@@ -0,0 +1,13 @@
+export const LoginStart = (userCredentials) => ({
+    type: 'LOGIN_START',
+});
+
+export const LoginSuccess = (user) => ({
+    type: 'LOGIN_SUCESS',
+    payload: user,
+});
+
+export const LoginFailure = (error) => ({
+    type: 'LOGIN_FAILURE',
+    payload: error,
+});
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,26 +1,48 @@
-import "./login.css"
+import { useRef } from 'react';
+import './login.css';
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(email.current.value);
+    };
+
     return (
-        <div className="login flex-align">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Lamasocial</h3>
-                    <span className="loginDesc">
+        <div className='login flex-align'>
+            <div className='loginWrapper'>
+                <div className='loginLeft'>
+                    <h3 className='loginLogo'>Lamasocial</h3>
+                    <span className='loginDesc'>
                         Connect with friends and the world around you on Lamasocial.
                     </span>
                 </div>
 
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <button className="loginButton">Log In</button>
-                        <span className="loginForgot">Forgot Password?</span>
-                        <button className="loginRegisterButton">Create a new Account</button>
-                    </div>
+                <div className='loginRight'>
+                    <form className='loginBox' onSubmit={handleClick}>
+                        <input
+                            placeholder='Email'
+                            type='email'
+                            required
+                            className='loginInput'
+                            ref={email}
+                        />
+                        <input
+                            placeholder='Password'
+                            type='password'
+                            required
+                            minLength={6}
+                            className='loginInput'
+                            ref={password}
+                        />
+                        <button className='loginButton'>Log In</button>
+                        <span className='loginForgot'>Forgot Password?</span>
+                        <button className='loginRegisterButton'>Create a new Account</button>
+                    </form>
                 </div>
             </div>
         </div>
-    )
+    );
 }
