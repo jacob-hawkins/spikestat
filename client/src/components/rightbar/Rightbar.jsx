f30fef5248cc80778e336b5e8b2ab9b0010b65f1
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-06-07 21:28:55 -0400

    rightbar is broken

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,27 @@
-import './rightbar.css';
+import { useEffect, useState } from 'react';
 import { Users } from '../../dummyData';
 import Online from '../online/Online';
+import axios from 'axios';
+import './rightbar.css';
 
 export default function Rightbar({ user }) {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const [friends, setFriends] = useState([]);
+
+    console.log(user);
+
+    // useEffect(() => {
+    // const getFriends = async () => {
+    //     try {
+    //         // const friendList = await axios.get('/users/friends/' + user._id);
+    //         // setFriends(friendList.data);
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
+    // getFriends();
+    // }, [user._id]);
+
     const HomeRightbar = () => {
         return (
             <>
@@ -10,7 +29,7 @@ export default function Rightbar({ user }) {
                     <img className='birthdayImg' src='/assets/gift.png' alt='' />
 
                     <span className='birthdayText'>
-                        <span className='bold'>Pola Foster</span> and{' '}
+                        <span className='bold'>Pola Foster</span> and
                         <span className='bold'>3 other friends</span> have a birthday today.
                     </span>
                 </div>
@@ -29,8 +48,6 @@ export default function Rightbar({ user }) {
     };
 
     const ProfileRightbar = () => {
-        const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-
         return (
             <>
                 <h4 className='rightbarTitle'>User information</h4>
@@ -59,30 +76,20 @@ export default function Rightbar({ user }) {
 
                 <h4 className='rightbarTitle'>User friends</h4>
                 <div className='rightbarFollowing'>
-                    <div className='rightbarFollower flex-align'>
-                        <img className='rightbarFollowerImg' src={`${PF}person/1.jpeg`} alt='' />
-                        <span className='rightbarFollowerName'>John Carter</span>
-                    </div>
-
-                    <div className='rightbarFollower flex-align'>
-                        <img className='rightbarFollowerImg' src={`${PF}person/1.jpeg`} alt='' />
-                        <span className='rightbarFollowerName'>John Carter</span>
-                    </div>
-
-                    <div className='rightbarFollower flex-align'>
-                        <img className='rightbarFollowerImg' src={`${PF}person/1.jpeg`} alt='' />
-                        <span className='rightbarFollowerName'>John Carter</span>
-                    </div>
-
-                    <div className='rightbarFollower flex-align'>
-                        <img className='rightbarFollowerImg' src={`${PF}person/1.jpeg`} alt='' />
-                        <span className='rightbarFollowerName'>John Carter</span>
-                    </div>
-
-                    <div className='rightbarFollower flex-align'>
-                        <img className='rightbarFollowerImg' src={`${PF}person/1.jpeg`} alt='' />
-                        <span className='rightbarFollowerName'>John Carter</span>
-                    </div>
+                    {/* {friends.map((friend) => (
+                        <div className='rightbarFollower flex-align'>
+                            <img
+                                className='rightbarFollowerImg'
+                                src={
+                                    friend.profilePicture
+                                        ? PF + friend.profilePicture
+                                        : PF + 'person/noAvatar.png'
+                                }
+                                alt=''
+                            />
+                            <span className='rightbarFollowerName'>{friend.username}</span>
+                        </div>
+                    ))} */}
                 </div>
             </>
         );
