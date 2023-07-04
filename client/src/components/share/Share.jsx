3f4c4817ce95970528c0bfdb60ae3da5425fc4d8
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-07-04 01:14:22 -0400

    location field added to share

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -10,12 +10,14 @@ export default function Share() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     const { user } = useContext(AuthContext);
     const desc = useRef();
+    const location = useRef();
 
     const submitHandler = async (e) => {
         e.preventDefault();
         const newPost = {
             userId: user._id,
             desc: desc.current.value,
+            location: location.current.value,
         };
 
         try {
@@ -49,21 +51,24 @@ export default function Share() {
                 <form className='shareBottom flex-align' onSubmit={submitHandler}>
                     <div className='shareOptions'>
                         <Tooltip title='Create a Game'>
-                            <IconButton className='flex-align'>
+                            <IconButton>
                                 <AddBox style={{ fontSize: 30 }} />
                             </IconButton>
                         </Tooltip>
 
-                        <Tooltip title='Add Location'>
-                            <IconButton className='flex-align'>
-                                <Room style={{ fontSize: 30 }} />
-                            </IconButton>
-                        </Tooltip>
+                        <Room style={{ fontSize: 30 }} />
+
+                        <input
+                            type='text'
+                            id='shareLocationInput'
+                            placeholder='Location'
+                            ref={location}
+                        />
                     </div>
 
                     <div className='shareButton'>
                         <Tooltip title='Post'>
-                            <IconButton type='submit' className='flex-align'>
+                            <IconButton type='submit'>
                                 <Send style={{ fontSize: 30 }} />
                             </IconButton>
                         </Tooltip>
