8fa22a00734394a1f25681b6b1c2c5bef36fb899
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-07-23 16:38:57 -0400

    login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { useContext, useRef } from 'react';
 import { loginCall } from '../../apiCalls';
 import './login.css';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     const email = useRef();
@@ -17,42 +18,40 @@ export default function Login() {
     return (
         <div className='login flex-align'>
             <div className='loginWrapper'>
-                <div className='loginLeft'>
-                    <h3 className='loginLogo'>Lamasocial</h3>
-                    <span className='loginDesc'>
-                        Connect with friends and the world around you on Lamasocial.
-                    </span>
-                </div>
+                <div className='loginLeft'></div>
 
                 <div className='loginRight'>
-                    <form className='loginBox' onSubmit={handleClick}>
-                        <input
-                            placeholder='Email'
-                            type='email'
-                            required
-                            className='loginInput'
-                            ref={email}
-                        />
-                        <input
-                            placeholder='Password'
-                            type='password'
-                            required
-                            minLength={6}
-                            className='loginInput'
-                            ref={password}
-                        />
-                        <button className='loginButton' type='submit' disabled={isFetching}>
-                            {isFetching ? <CircularProgress color='inherit' /> : 'Log In'}
-                        </button>
-                        <span className='loginForgot'>Forgot Password?</span>
-                        <button className='loginRegisterButton'>
-                            {isFetching ? (
-                                <CircularProgress color='inherit' />
-                            ) : (
-                                'Create a new Account'
-                            )}
-                        </button>
-                    </form>
+                    <h1 className='loginLogo'>SpikeStat</h1>
+
+                    <div className='loginBox'>
+                        <form className='loginBox' onSubmit={handleClick}>
+                            <input
+                                placeholder='Email'
+                                type='email'
+                                required
+                                className='loginInput'
+                                ref={email}
+                            />
+                            <input
+                                placeholder='Password'
+                                type='password'
+                                required
+                                minLength={6}
+                                className='loginInput'
+                                ref={password}
+                            />
+                            <button className='loginButton' type='submit' disabled={isFetching}>
+                                {isFetching ? <CircularProgress color='inherit' /> : 'Log In'}
+                            </button>
+                        </form>
+
+                        <div className='loginRegister'>
+                            <span>Don't have an account?</span>
+                            <Link to={'/register'} className='loginRegisterLink'>
+                                Sign up
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
